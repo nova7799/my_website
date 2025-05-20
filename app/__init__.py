f5d432cc0b4f4beabb67ff24ce8a274962707ea2
nova7799 <velfavelavan143@gmail.com>
2025-05-20 13:10:37 +0530

    Fix merge conflict in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,42 +1,13 @@
-<<<<<<< HEAD
+
 from flask import Flask
-=======
-# from flask import Flask
-# from dotenv import load_dotenv
-# from pathlib import Path
-# import os
-
-# # Load environment variables
-# load_dotenv(os.path.join(os.path.dirname(__file__), '..', 'project.env'))
-# env_path = Path(__file__).parent.parent / 'project.env'
-# load_dotenv(env_path)
-
-
-# app = Flask(__name__)
-# app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'dev-fallback-key')
-
-
-
-# from .routes import register_routes
-# register_routes(app)
-
-
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
->>>>>>> 9a5e7ae3bf8d5382f1a7c4ff1d5c230cbb09ec86
+
 from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-# Load environment variables
-<<<<<<< HEAD
+
 load_dotenv(os.path.join(os.path.dirname(__file__), '..', 'project.env'))
 env_path = Path(__file__).parent.parent / 'project.env'
 load_dotenv(env_path)
@@ -49,7 +20,7 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'dev-fallback-key')
 
 from .routes import register_routes
 register_routes(app)
-=======
+
 env_path = Path(__file__).parent.parent / 'project.env'
 load_dotenv(env_path)
 
@@ -69,4 +40,4 @@ def create_app():
     register_routes(app)
 
     return app
->>>>>>> 9a5e7ae3bf8d5382f1a7c4ff1d5c230cbb09ec86
+
